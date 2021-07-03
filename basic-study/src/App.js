0449f44eddc718094124d1a8d7f1754843b39ece
yangdahyun <govl1535@naver.com>
2021-07-03 17:36:20 +0900

    배열에 항목 추가하기

--- basic-study/src/App.js
+++ basic-study/src/App.js
@@ -1,8 +1,21 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
+import CreateUser from './CreateUser';
 import UsersList from './User_list';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:'',
+  });
+  const {username, email} = inputs;
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+  const [users, setUsers] = useState  ([
     {
         id:1,
         username: 'yang',
@@ -18,15 +31,30 @@ function App() {
         username: 'hyun',
         email: 'hyun@naver.com'
     }
-];
+]);
   const nextId = useRef(4);
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    //setUsers([...users, user]);
+    setUsers(users.concat(user));
+    // push 업데이트 되지 않음 사용 x
+    setInputs({
+      username:'',
+      email:'',
+    });
     //console.log(nextId.current); //4
     nextId.current += 1;
   }
 
   return (
+    <>
+    <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
     <UsersList users={users}  />
+    </>
   );
 }
 
