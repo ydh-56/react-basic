fbb902180a78edffd07926c635a9223108b633ea
ydh <govl1535@naver.com>
2021-07-07 16:04:06 +0900

    immer 구현

--- basic-study/src/App.js
+++ basic-study/src/App.js
@@ -2,6 +2,7 @@ import React, {useRef, useReducer, useMemo, useCallback, createContext} from 're
 import CreateUser from './CreateUser';
 import UsersList from './User_list';
 import useInputs from './useInputs';
+import produce from 'immer';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중')
@@ -34,24 +35,35 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
       case 'CREATE_USER':
-        return {
-          inputs: initialState.inputs,
-          users: state.users.concat(action.user)
-        }
+        return produce(state, draft => {
+          draft.users.push(action.user);
+        })
+        // return {
+        //   inputs: initialState.inputs,
+        //   users: state.users.concat(action.user)
+        // }
       case 'TOGGLE_USER':
-        return {
-          ...state,
-          users: state.users.map(user => 
-              user.id === action.id
-                ? {...user, active: !user.active}
-                : user
-            )
-        };
+        return produce(state, draft => {
+          const user = draft.users.find(user => user.id === action.id);
+          user.active = !user.active;
+        })
+        // return {
+        //   ...state,
+        //   users: state.users.map(user => 
+        //       user.id === action.id
+        //         ? {...user, active: !user.active}
+        //         : user
+        //     )
+        // };
       case 'REMOVE_USER':
-        return {
-          ...state,
-          users: state.users.filter(user => user.id !== action.id)
-        }
+        return produce(state, draft => {
+          const index = draft.users.findIndex(user => user.id === action.id);
+          draft.users.splice(index, 1);
+        });
+        // return {
+        //   ...state,
+        //   users: state.users.filter(user => user.id !== action.id)
+        // }
       default:
         return state;
   }
